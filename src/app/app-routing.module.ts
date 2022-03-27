b1aad1eab685c4303ab0db11359e69b8fe21f37c
Vais2910 <86576044+Vais30@users.noreply.github.com>
2022-03-27 16:01:16 +0300

    Корневой модуль с роутингом и компонентом

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: 'client',
+    loadChildren: () => import('./client/client.module').then(m => m.ClientModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'client',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
