75aa6c208296c0cc354a682ced50a62558ddaaf2
Vais2910 <86576044+Vais30@users.noreply.github.com>
2022-04-23 23:31:57 +0300

    Корректировка мобильной вёрстки и моделей

--- src/app/_models/dialog.ts
+++ src/app/_models/dialog.ts
@@ -1,7 +1,5 @@
 import {Message} from "./message";
 
 export class Dialog {
-  // send_from_id: number;
-  // send_to_id: number;
   messages: Message[];
 }
--- src/app/_models/message.ts
+++ src/app/_models/message.ts
@@ -2,7 +2,7 @@ export class Message {
   id: string;
   message_text: string;
   timestamp: string;
-  send_from_id: number;
-  send_to_id: number;
+  send_from_id: string;
+  send_to_id: string;
   is_read: boolean;
 }
